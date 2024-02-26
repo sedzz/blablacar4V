1045202bb6bca966f381ed97d6306cd840cf57b6
Sebas Diez <infernus10456ps3d@gmail.com>
2024-02-26 13:25:51 +0100

    Mapa integrado en la aplicacion intent

--- app/src/main/java/org/cuatrovientos/blabla4v/activities/MainActivity.java
+++ app/src/main/java/org/cuatrovientos/blabla4v/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package org.cuatrovientos.blabla4v.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.cuatrovientos.blabla4v.R;
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button); // Suponiendo que tienes un botón en tu layout con id "button"
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para iniciar SecondActivity
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
